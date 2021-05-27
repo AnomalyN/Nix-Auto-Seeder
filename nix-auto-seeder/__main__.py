8a24a59271bf72de1a53f46ce05f07a364cd3442
RoyK <rkokk@protonmail.com>
2021-05-27 07:37:55 +0200

    feat: update __main__.py to use distro files

--- nix-auto-seeder/__main__.py
+++ nix-auto-seeder/__main__.py
@@ -1,7 +1,8 @@
-import sys
 import util
-import inspect
 import click
+import itertools
+
+from pathlib import Path
 
 from loguru import logger
 
@@ -9,12 +10,24 @@ from loguru import logger
 @click.command()
 @click.option('-o', '--output-dir',
               help='Save torrents in dir instead of submitting to deluge',
-              type=click.Path(file_okay=False, writable=True))
+              type=click.Path(file_okay=False, writable=True, path_type=Path))
 def main(output_dir):
+    distro_files = []
 
     for distro in util.get_distros():
-        print(distro)
-        distro.get_torrents()
+        logger.info("Parsing {}", distro)
+        distro_files.append(distro.get_torrents())
+
+    # Ensure output dir exists if defined
+    if output_dir and not output_dir.exists():
+        output_dir.mkdir()
+
+    for (filename, t_file) in itertools.chain(*distro_files):
+
+        # Store copies of torrent files in output_dir
+        if output_dir:
+            util.copy_file(output_dir, filename, t_file)
+
 
 if __name__ == '__main__':
     main()
