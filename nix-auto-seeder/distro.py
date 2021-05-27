68f3d3d3d76b9886e63cf67eb7d32752960b91cd
RoyK <rkokk@protonmail.com>
2021-05-27 07:36:55 +0200

    feat: add torrent download funcs in Distro
    This add specific functions for gathering torrent files from paths.
    Gathering the files and paths is seperated in multiple functions so that
    specific functions could be overloaded by distro specific instances,
    e.g. for Ubuntu.

--- nix-auto-seeder/distro.py
+++ nix-auto-seeder/distro.py
@@ -1,8 +1,10 @@
-import util
+import tempfile
 
 from ftplib import FTP
 
 from loguru import logger
+
+import util
 
 
 class Distro:
@@ -16,9 +18,32 @@ class Distro:
     def get_torrents(self):
         with FTP(self.server, self.user, self.pw) as ftp:
 
-            for path in self.paths:
-                for f in ftp.nlst(path):
-                    print(f)
+            torrent_files = self.get_torrents_paths(ftp)
+            logger.info("Got {} torrent_files from {}", len(torrent_files), self)
+
+            for t_file in self.get_torrents_files(ftp, torrent_files):
+                yield t_file
+
+    def get_torrents_paths(self, ftp):
+        for path in self.paths:
+            files = ftp.nlst(path)
+            logger.debug("Identified {} files", len(files))
+
+            # Filter files to only list torrent files
+            torrent_files = list(filter(util.verify_torrents, files))
+            return torrent_files
+
+    def get_torrents_files(self, ftp, torrent_files):
+        for t_file in torrent_files:
+            filename = t_file.split('/')[-1]
+            tmp_file = tempfile.SpooledTemporaryFile()
+
+            try:
+                ftp.retrbinary(f"RETR {t_file}", tmp_file.write, 8*1024)
+                yield (filename, tmp_file)
+            except Exception as e:
+                logger.exception(e)
+
 
     def __str__(self):
         return f"{self.__class__.__name__} Distro"
