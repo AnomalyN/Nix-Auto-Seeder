bb3ae1a00ef5e04c4fe5bdd30ecf794cdffc0b2b
RoyK <rkokk@protonmail.com>
2021-05-31 22:41:06 +0200

    chore: set distros to use new FTPDistro class

--- nix-auto-seeder/distros/centos.py
+++ nix-auto-seeder/distros/centos.py
@@ -1,12 +1,16 @@
-from distro import Distro
+from distro import FTPDistro
 
 
-class CentOS(Distro):
+class CentOS(FTPDistro):
 
     def __init__(self):
         super().__init__(
             server='ftp.mirrorservice.org',
-            paths=[
-                'sites/mirror.centos.org/7/isos/x86_64/'
+            paths=
+            [
+                {
+                    '/sites/mirror.centos.org/': r'(?P<major>\d+)\.(?P<minor>\d+[\.\d]*)',
+                    '/isos/': r'(?P<arch>\w+)'
+                },
             ]
         )
--- nix-auto-seeder/distros/ubuntu.py
+++ nix-auto-seeder/distros/ubuntu.py
@@ -1,54 +1,15 @@
-import re
-import itertools
-
-from loguru import logger
-
-import util
-from distro import Distro
+from distro import FTPDistro
 
 
-class Ubuntu(Distro):
+class Ubuntu(FTPDistro):
 
     def __init__(self):
-        self.version_re = re.compile(r'\d+\.\d+(\.\d+)?')
-
         super().__init__(
             server='ftp.mirrorservice.org',
             paths=[
-                'sites/releases.ubuntu.com/'
+                {
+                    'sites/releases.ubuntu.com/': r'(?P<major>\d+\.\d+)\.(?P<minor>\d+)?',
+                    '.': r'(?P<arch>\w+)\.iso\.torrent'
+                }
             ]
         )
-
-    def get_torrents_paths(self, ftp, num_releases=1):
-        torrent_files = []
-
-        for path in self.paths:
-            releases = dict()
-            releases_raw = ftp.nlst(path)
-
-            for release in releases_raw:
-                match = self.version_re.search(release)
-
-                # If no match, then dir is not related to Ubuntu release
-                if not match:
-                    continue
-
-                # Get version info
-                version = match.group()
-                releases[version] = release
-
-            # Sort releases by version
-            releases = {k: v for k, v in sorted(releases.items(), key=lambda i: i[0], reverse=True)}
-            logger.debug("Found Ubuntu releases: {}", ', '.join((releases.keys())))
-
-            # if num_releases is 0, get all versions
-            if num_releases:
-                releases = itertools.islice(releases.items(), num_releases)
-
-            for version, path in releases:
-                files = ftp.nlst(path)
-                logger.trace("[{}] {} FTP files: {}", self, path, files)
-                torrent_files += list(filter(util.verify_torrents, files))
-
-        logger.trace("[{}] FTP torrent files: {}", self, torrent_files)
-        return torrent_files
