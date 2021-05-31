7a44ebd76ff77b84fab8bccc63c0ae4897b12814
RoyK <rkokk@protonmail.com>
2021-05-31 22:40:30 +0200

    feat: extend util.py
    * Add helper functions
    * Extend loguru configuration

--- nix-auto-seeder/util.py
+++ nix-auto-seeder/util.py
@@ -1,9 +1,13 @@
+import os
+import re
 import imp
 import sys
 import inspect
 import tempfile
+import itertools
 import subprocess
 
+from typing import List
 from pathlib import Path
 
 from loguru import logger
@@ -21,13 +25,15 @@ def get_distros(dir='distros'):
                 yield obj()
 
 
-def verify_torrents(filename):
-    return filename.endswith('.torrent')
+def filter_torrent_files(files: list, ext: str = '.torrent') -> List[str]:
+    """ Select torrent files from list of files
 
-
-def copy_file(f_input, f_output):
-    f_input.seek(0)  # Ensure input file is read from beginning
-    f_output.write(f_input.read())
+    :param list: list of filenames to filter
+    :param ext: extension to verify whether file is torrent file (default `.torrent`)
+    :return list: list of filenames which ends with extension
+    """
+    for entry in filter(lambda f: f.endswith(ext), files):
+        yield entry
 
 
 def run_deluge(t_file):
@@ -53,10 +59,12 @@ def setup_loguru(verbose):
     format = str(
         "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | "
         "<level>{level: <8}</level> | "
+        "<cyan>{extra[distro]}</cyan> | "
         "<level>{message}</level>",
     )
 
     logger.remove()
+    logger.configure(extra={'distro': ''})
 
     # Set logging level based on verbose level
     if verbose == 0:
@@ -65,3 +73,30 @@ def setup_loguru(verbose):
         logger.add(sys.stderr, format=format, level="DEBUG", diagnose=False, backtrace=True)
     elif verbose >= 2:
         logger.add(sys.stderr, format=format, level="TRACE", diagnose=True, backtrace=True)
+
+
+def filter_releases(releases, num=0):
+    releases = {k: v for k, v in sorted(releases.items(), key=lambda i: i[0], reverse=True)}
+
+    # if num_releases is 0, get all versions
+    if num:
+        releases = itertools.islice(releases.items(), num)
+
+    return releases
+
+
+def is_directory(path: str) -> bool:
+    """ Verify if path is directory or file
+
+    :param: path to verify
+    :return: path is directory
+    """
+    (head, filename) = os.path.split(path)
+
+    if not filename:
+        return True
+
+    else:
+        (path, ext) = os.path.splitext(filename)
+
+        return not bool(ext) or (bool(ext) and bool(re.match(r'\.?\d+', ext)))
